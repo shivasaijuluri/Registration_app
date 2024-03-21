8d5ae68972d84457fa27e2edb37efc9d2f714a17
Mr. Shiva <shivasaijuluri2004@gmail.com>
2024-03-21 20:13:23 +0530

    updated notification

--- app/src/main/java/com/example/regfire/notification.java
+++ app/src/main/java/com/example/regfire/notification.java
@@ -1,17 +1,18 @@
 package com.example.regfire;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-
 public class notification extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView t =findViewById(R.id.notificataion_text);
-        String n=getIntent().getStringExtra("Message");
+        setContentView(R.layout.activity_notification); // Inflate the layout
+
+        TextView t = findViewById(R.id.notificataion_text); // Corrected ID
+        String n = getIntent().getStringExtra("message"); // Corrected key
         t.setText(n);
     }
 }
