1b020186028e295fb7a1449d94803c698572d15f
Mr. Shiva <shivasaijuluri2004@gmail.com>
2024-04-12 12:40:26 +0530

    added spinner

--- app/src/main/java/com/example/regfire/FriendsActivity.java
+++ app/src/main/java/com/example/regfire/FriendsActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.app.ProgressDialog;
 import android.widget.Toast;
@@ -84,5 +86,10 @@ public class FriendsActivity extends AppCompatActivity {
                 }).start();
             }
         });
+
+        Spinner spinner=(Spinner) findViewById(R.id.country_spinner);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.country_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
     }
 }
